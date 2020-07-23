89f5370d4e8f016c2324bd9cc22073ba00827606
ARTurchinovich <turchinovichar@gmail.com>
2020-07-23 14:00:31 +0300

    Main,Message repository, message added

--- src/main/java/com/example/sweater/GreetingСontroller.java
+++ src/main/java/com/example/sweater/GreetingСontroller.java
@@ -1,13 +1,19 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingСontroller {
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name,
@@ -18,7 +24,23 @@ public class GreetingСontroller {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some","hello, letsCode!");
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
         return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+
     }
 }
